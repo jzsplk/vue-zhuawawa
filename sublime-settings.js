601be5386bac244a4f7cd10a64fb1ec115f075fa
Alex <jzsplking@gmail.com>
2018-09-28 00:20:42 +0800

    Create sublime-settings.js

---
+++ sublime-settings.js
@@ -0,0 +1,58 @@
+{
+	"always_show_minimap_viewport": true,
+	"auto_find_in_selection": true,
+	"bold_folder_labels": false,
+	"caret_style": "wide",
+	"close_windows_when_empty": false,
+	"color_scheme": "Packages/Color Scheme - Default/Monokai.sublime-color-scheme",
+	"copy_with_empty_selection": false,
+	"create_window_at_startup": false,
+	"detect_indentation": true,
+	"dictionary": "Packages/Language - English/en_GB.dic",
+	"drag_text": false,
+	"file_exclude_patterns":
+	[
+		".DS_Store"
+	],
+	"folder_exclude_patterns":
+	[
+		".bundle",
+		".git",
+		".hg",
+		".sass-cache",
+		".svn",
+		"bin",
+		"CVS",
+		"tmp"
+	],
+	"font_face": "Monaco",
+	"font_options":
+	[
+		"subpixel_antialias"
+	],
+	"font_size": 13,
+	"highlight_line": true,
+	"highlight_modified_tabs": true,
+	"ignored_packages":
+	[
+		"Vintage"
+	],
+	"indent_guide_options":
+	[
+		"draw_normal",
+		"draw_active"
+	],
+	"line_padding_bottom": 3,
+	"line_padding_top": 3,
+	"match_brackets_angle": true,
+	"material_theme_accent_orange": true,
+	"material_theme_compact_sidebar": true,
+	"material_theme_panel_separator": true,
+	"material_theme_small_tab": true,
+	"overlay_scroll_bars": "enabled",
+	"tab_size": 2,
+	"theme": "Material-Theme.sublime-theme",
+	"translate_tabs_to_spaces": true,
+	"trim_trailing_white_space_on_save": true,
+	"word_wrap": true
+}
